d61e19dc68b4e5421dc960d37e8e143dc5c55e58
Amir <amir.34gl3@gmail.com>
2020-09-25 11:31:56 +0330

    Added food_nature field to Plan model

--- plans/models.py
+++ plans/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth import get_user_model
 from accounts.models import Coach, Client
 from sizes.models import Size
 
+FOOD_NATURE_CHOICES = (
+    ("VEGETARIAN", "گیاه‌خوار"),
+    ("NONVEGETARIAN",  "با خوردن گوشت مشکلی ندارم.")
+)
+
 
 class Plan(models.Model):
     client = models.OneToOneField(
@@ -11,6 +16,8 @@ class Plan(models.Model):
     coach = models.OneToOneField(
         Coach, on_delete=models.DO_NOTHING, related_name="coach")
     size = models.OneToOneField(Size, on_delete=models.DO_NOTHING, null=True)
+    food_nature = models.CharField(
+        max_length=13, choices=FOOD_NATURE_CHOICES, default="NONVEGETARIAN")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
