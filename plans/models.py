45bfd68459f2ec8a5491b675f01e48f605b93802
Amir <amir.34gl3@gmail.com>
2020-09-25 10:41:01 +0330

    Added size field to Plan model

--- plans/models.py
+++ plans/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 from accounts.models import Coach, Client
+from sizes.models import Size
 
 
 class Plan(models.Model):
-    client = models.OneToOneField(Client, on_delete=models.DO_NOTHING, related_name="client")
-    coach = models.OneToOneField(Coach, on_delete=models.DO_NOTHING, related_name="coach")
+    client = models.OneToOneField(
+        Client, on_delete=models.DO_NOTHING, related_name="client")
+    coach = models.OneToOneField(
+        Coach, on_delete=models.DO_NOTHING, related_name="coach")
+    size = models.OneToOneField(Size, on_delete=models.DO_NOTHING, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.client.email + self.coach.email
+        return self.client.email
