6549c11587f2b1a77bfde9ff80fe0421d6f9c3ed
Amir <amir.34gl3@gmail.com>
2020-09-25 13:00:45 +0330

    Created ClientImage model

--- client_images/admin.py
+++ client_images/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ClientImage
+
+admin.site.register(ClientImage)
--- client_images/models.py
+++ client_images/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class ClientImage(models.Model):
+    front = models.ImageField(upload_to="")
+    side = models.ImageField(upload_to="")
+    back = models.ImageField(upload_to="")
