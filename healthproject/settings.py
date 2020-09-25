25fd70f58df60600906bfc728d43231c173e6108
Amir <amir.34gl3@gmail.com>
2020-09-25 10:33:43 +0330

    Created sizes app and it's model

--- healthproject/settings.py
+++ healthproject/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "profiles",
     "accounts.apps.AccountsConfig",
     "plans",
+    "sizes",
 
     # third parties
     "django_extensions"
---
+++ sizes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from accounts.models import Client
+
+
+class Size(models.Model):
+    client = models.OneToOneField(
+        Client,
+        on_delete=models.CASCADE
+    )
+    height = models.CharField(max_length=30, blank=False, null=False)
+    weight = models.CharField(max_length=30, blank=False, null=False)
+    neck = models.CharField(max_length=30, blank=False, null=False)
+    arm = models.CharField(max_length=30, blank=False, null=False)
+    wrist = models.CharField(max_length=30, blank=False, null=False)
+    waist = models.CharField(max_length=30, blank=False, null=False)
+    hamstring = models.CharField(max_length=30, blank=False, null=False)
+    calf = models.CharField(max_length=30, blank=False, null=False)
+
+    def __str__(self):
+        return self.client.email + self.client.username
