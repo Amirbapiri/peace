b965d23602477ddd1cff23460724480c7bf2061a
Amir <amir.34gl3@gmail.com>
2020-10-01 21:06:57 +0330

    Display number of plans for a specific coach

--- coaches/views.py
+++ coaches/views.py
@@ -1,5 +1,14 @@
+import datetime
+
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from plans.models import Plan
 
+@login_required(login_url="accounts:login")
 def dashboard(request):
-    return render(request, "coaches/dashboard.html", {})
+    plans_num = Plan.objects.filter(coach=request.user).count()
+    context = {
+        "plans_num": plans_num
+    }
+    return render(request, "coaches/dashboard.html", context)
