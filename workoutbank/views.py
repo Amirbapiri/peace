5e4a2e6f187a63d514b2fb9cce52ed78be4a7eda
amir <amirbapiri@gmail.com>
2020-10-16 15:12:55 +0000

    Created workout_detail to get information of a specific workout

--- workoutbank/views.py
+++ workoutbank/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from workoutbank.models import WorkoutItem
 
@@ -9,3 +9,11 @@ def workout_list(request):
         "workouts": workouts,
     }
     return render(request, "workoutbank/workoutlist.html", context)
+
+
+def workout_detail(request, workout_id):
+    workout = get_object_or_404(WorkoutItem, pk=workout_id)
+    context = {
+        "workout": workout
+    }
+    return render(request, "workoutbank/workoutdetail.html", context)
